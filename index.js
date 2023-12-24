42cf090256988bf14ba6f04e277bb48bdbab4251
johnbarsigo <john.barsigo@student.moringaschool.com>
2023-12-24 14:26:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution in this file!
+let employee = {
+    name : "Sam",
+    streetAddress : "00200 NBO"
+}
+
+//NO.1 
+
+ function updateEmployeeWithKeyAndValue ( employee, key, value){
+    return {
+        ...employee,
+        [key]: value
+      };
+ }
+ const newEmployee = updateEmployeeWithKeyAndValue ( employee, "location", "Nakuru" );
+ console.log ( newEmployee );
+
+//NO.2
+
+ function destructivelyUpdateEmployeeWithKeyAndValue ( employee, key, value ){
+    employee [key] = value;
+    return employee;
+ }
+ const newEmployee1 = destructivelyUpdateEmployeeWithKeyAndValue ( "Alton", "phone", "7178090" )
+ console.log ( newEmployee1 );
+ 
+ //NO.3
+
+ function deleteFromEmployeeByKey(employee, key) {
+    const { [key]: deletedKey, ...newEmployee2 } = employee;
+    return newEmployee2;
+    // let newEmployee2 = { ...employee };
+    // delete newEmployee2.key;
+  }
+
+//NO.4
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete( employee.key);
+    return employee;
+}
+console.log ( employee );
+
